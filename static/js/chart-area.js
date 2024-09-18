9804976efa28effaab6d86d0484d58d86fabb3ed
Victor Omuya <viczik16@gmail.com>
2024-09-18 15:51:50 +0100

    updates

--- static/js/chart-area.js
+++ static/js/chart-area.js
@@ -34,7 +34,7 @@ var myLineChart = new Chart(ctx, {
   data: {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [{
-      label: "Earnings",
+      label: "Sales",
       lineTension: 0.3,
       backgroundColor: "rgba(78, 115, 223, 0.05)",
       borderColor: "rgba(78, 115, 223, 1)",
@@ -81,7 +81,7 @@ var myLineChart = new Chart(ctx, {
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function (value, index, values) {
-            return number_format(value) + ' $';
+            return number_format(value) + ' N';
           }
         },
         gridLines: {
@@ -113,7 +113,7 @@ var myLineChart = new Chart(ctx, {
       callbacks: {
         label: function (tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ": " + number_format(tooltipItem.yLabel) + ' $';
+          return datasetLabel + ": " + number_format(tooltipItem.yLabel) + ' N';
         }
       }
     }
